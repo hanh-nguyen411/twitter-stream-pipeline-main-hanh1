bc93a8dfa06587dfec65ee5123749725acffed56
hanh-nguyen411 <88958173+hanh-nguyen411@users.noreply.github.com>
2023-03-11 20:06:30 +0100

    Update __init__.py

--- kafka_config/__init__.py
+++ kafka_config/__init__.py
@@ -1,15 +1,15 @@
 import os
 
 
-# AUTHENTICATION
-API_KEY = os.getenv('API_KEY', "OBK7AL57JANLSTYJ")
-ENDPOINT_SCHEMA_URL = os.getenv('ENDPOINT_SCHEMA_URL', "https://psrc-kk5gg.europe-west3.gcp.confluent.cloud")
-API_SECRET_KEY = os.getenv('API_SECRET_KEY', "oVu6PhV4kJDhHgEsCw6z7z/Gdhzk8wnyCYmC2+OJyCRSanKbOPl8+m//o5HTddck")
-BOOTSTRAP_SERVER = os.getenv('BOOTSTRAP_SERVER', "pkc-lzoyy.europe-west6.gcp.confluent.cloud:9092")
-SECURITY_PROTOCOL = os.getenv('SECURITY_PROTOCOL', "SASL_SSL")
-SSL_MECHANISM = os.getenv('SSL_MECHANISM', "PLAIN")
-SCHEMA_REGISTRY_API_KEY = os.getenv('SCHEMA_REGISTRY_API_KEY', 'PG4TJGIR7ZVIA5ET')
-SCHEMA_REGISTRY_API_SECRET = os.getenv('SCHEMA_REGISTRY_API_SECRET', '2Xns46ev80VDO4I/DY3NMvGfItsrvNkkmEI57yFu9syMCH8Zzbi+GivsQaY2bL92')
+# AUTHENTICATION  - Fill the info in below
+API_KEY = os.getenv('API_KEY', "")
+ENDPOINT_SCHEMA_URL = os.getenv('ENDPOINT_SCHEMA_URL', "")
+API_SECRET_KEY = os.getenv('API_SECRET_KEY', "")
+BOOTSTRAP_SERVER = os.getenv('BOOTSTRAP_SERVER', "")
+SECURITY_PROTOCOL = os.getenv('SECURITY_PROTOCOL', "")
+SSL_MECHANISM = os.getenv('SSL_MECHANISM', "")
+SCHEMA_REGISTRY_API_KEY = os.getenv('SCHEMA_REGISTRY_API_KEY', '')
+SCHEMA_REGISTRY_API_SECRET = os.getenv('SCHEMA_REGISTRY_API_SECRET', '')
 
 # CONTENT
 TOPIC = os.getenv('TOPIC', 'tweets')
